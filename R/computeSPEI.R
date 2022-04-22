7a2e5777c359a033f58a9206865632059e1e1de0
Santiago Beguería <5500064+sbegueria@users.noreply.github.com>
2022-04-22 11:14:57 +0200

    Version 2.7.

--- R/computeSPEI.R
+++ R/computeSPEI.R
@@ -3,18 +3,19 @@
 # to disk on directory './outputNcdf/'.
 
 if (!require('pacman')) install.packages('pacman')
-pacman::p_load(SPEI, ncdf4, snowfall, parallel, Hmisc)
+pacman::p_load(ncdf4, snowfall, parallel, Hmisc) #SPEI
 source('./R/functions.R')
 
 # Init a parallel computing cluster; modify the parameter `cpus` to the
 # desired number of cores; otherwise, use all available cores
 #sfInit(parallel=TRUE, cpus=detectCores())
-sfInit(parallel=TRUE, cpus=4)
+sfInit(parallel=TRUE, cpus=10)
 sfExport(list='spei', namespace='SPEI')
 
 # Compute SPEI at all time scales between 1 and 48 and store to disk
-#for (i in c(1:48)) {
-for (i in c(1,3,6,12)) {
+for (i in c(1)) {
+#for (i in c(1,3,6,9,12)) {
+#for (i in c(1:48)[-c(1,3,6,9,12)]) {
     spei.nc(
       sca=i,
 		  inPre='./inputData/cru_ts4.05.1901.2020.pre.dat.nc',
@@ -33,3 +34,6 @@ for (i in c(1,3,6,12)) {
 
 # Stop the parallel cluster
 sfStop()
+
+# Session information
+sessionInfo()
