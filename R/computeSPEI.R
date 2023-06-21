76865b2b2a9db79b8b4aeae9a400d4da028f6cf7
Santiago Beguería <santiago.begueria@csic.es>
2023-06-21 13:41:37 +0200

    CRU TS 4.6
    Changing names and routes to input files.

--- R/computeSPEI.R
+++ R/computeSPEI.R
@@ -11,7 +11,7 @@ devtools::install_github('sbegueria/SPEI@v1.8.0')
 
 # A function to efficiently compute the SPEI over a large netCDF file (using
 # multiple cores).
-source('./R/functions.R')
+source('./functions.R')
 
 # Initialize a parallel computing cluster; modify the parameter `cpus` to the
 # desired number of cores; otherwise, it will use all available cores.
@@ -19,16 +19,16 @@ sfInit(parallel=TRUE, cpus=detectCores())
 sfExport(list='spei', namespace='SPEI')
 
 # Compute SPEI at all time scales between 1 and 48 and store to disk.
-for (i in c(1:48)) {
+for (i in c(1:48)) {#  for (i in c(6)) {
     spei.nc(
       sca=i,
-		  inPre='./inputData/cru_ts4.06.1901.2021.pre.dat.nc',
-		  inEtp='./inputData/cru_ts4.06.1901.2021.pet.dat.nc',
-		  outFile=paste('./outputNcdf/spei',
+		  inPre='../inputData/cru_ts4.06.1901.2021.pre.dat.nc',
+		  inEtp='../inputData/cru_ts4.06.1901.2021.pet.dat.nc',
+		  outFile=paste('../outputNcdf/spei',
 		              formatC(i, width=2, format='d', flag='0'),'.nc',sep=''),
 		  title=paste('Global ',i,'-month',
 		            ifelse(i==1,'','s'),' SPEI, z-values, 0.5 degree',sep=''),
-		  comment='Using CRU TS 4.05 precipitation and potential evapotranspiration data',
+		  comment='Using CRU TS 4.06 precipitation and potential evapotranspiration data',
 		  block=36,
 		  inMask=NA,
 		  tlapse=NA
